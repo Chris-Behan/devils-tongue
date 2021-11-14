6895d65c18d079acc62e6c0179a8a4c0505d0c70
Chris Behan <chrisbehan1997@gmail.com>
2021-11-14 14:27:33 -0700

    Add cli

--- cmd/devilstongue/main.go
+++ cmd/devilstongue/main.go
@@ -1,13 +1,48 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/Chris-Behan/devils-tongue/str"
 )
 
 func main() {
-	fmt.Println("Hello world!")
-	a := str.ReverseString("test")
-	fmt.Println(a)
+
+	flag.Usage = func() {
+		fmt.Println("Reverse a string or use the input and output flags to reverse the contents of a file.\nEx. devilstongue 'string to reverse' or devilstongue -input 'input.txt' -output 'reversed.txt'")
+		flag.PrintDefaults()
+	}
+	var inputFilename string
+	var outputFilename string
+	flag.StringVar(&inputFilename, "input", "", "Name of the input file whose contents you want to reverse. If this flag is used, you must also use the output flag.")
+	flag.StringVar(&outputFilename, "output", "", "Name of the output file you want to write the reversed contents to. If this flag is used, you must also use the input flag.")
+	flag.Parse()
+
+	if inputFilename == "" && outputFilename == "" {
+		positionalArgs := flag.Args()
+		if len(positionalArgs) != 1 {
+			fmt.Print("Expected a single string argument. Ex: 'string to reverse'")
+			return
+		}
+
+		inputString := positionalArgs[0]
+		reversedString := str.ReverseString(inputString)
+		fmt.Print(reversedString)
+	} else {
+		if inputFilename == "" {
+			fmt.Print("Please specify the name of the input file. Ex: '-input input.txt'")
+			return
+		}
+		if outputFilename == "" {
+			fmt.Print("Please specify the name of the output file. Ex: '-output output.txt'")
+			return
+		}
+		err := str.ReverseTextfileContents(inputFilename, outputFilename)
+		if err != nil {
+			fmt.Print(err)
+			return
+		}
+		fmt.Printf("Reversed the contents of %v and saved them in %v", inputFilename, outputFilename)
+	}
 }
